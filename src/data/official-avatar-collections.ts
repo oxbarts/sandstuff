3b252edeeef5e9252fbcd66d05af219d1f70e639
oxbarts <oxbarts@youdontneedmy.email>
2024-08-06 21:43:34 +0200

    Fix quotes.

--- src/data/official-avatar-collections.ts
+++ src/data/official-avatar-collections.ts
@@ -8,241 +8,241 @@ export type OfficialAvatarCollection = {
 
 export const collections: OfficialAvatarCollection[] = [
   {
-    name: "Jab We Met",
+    name: 'Jab We Met',
     chain: 137,
-    address: "0x0649a55263a165f34ecbcd8a8868a776a43a012a",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/jab-we-met/en/",
-    osUrl: "https://opensea.io/collection/???"
+    address: '0x0649a55263a165f34ecbcd8a8868a776a43a012a',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/jab-we-met/en/',
+    osUrl: 'https://opensea.io/collection/???'
   },
   {
-    name: "Solo Leveling: Unlimited Avatar Collection",
+    name: 'Solo Leveling: Unlimited Avatar Collection',
     chain: 137,
-    address: "0x44f57b0d879114724f8ce203248d3d95209f1923",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/solo-leveling/en/",
-    osUrl: "https://opensea.io/collection/avatarssololeveling"
+    address: '0x44f57b0d879114724f8ce203248d3d95209f1923',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/solo-leveling/en/',
+    osUrl: 'https://opensea.io/collection/avatarssololeveling'
   },
   {
-    name: "Rabbids Rockstars Avatar Collection",
+    name: 'Rabbids Rockstars Avatar Collection',
     chain: 137,
-    address: "0x8ec369b054e0b732676bdaf872bbdcfa02b9c6d4",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/rabbids-rockstars/en/",
-    osUrl: "https://opensea.io/collection/rabbids-rockstar"
+    address: '0x8ec369b054e0b732676bdaf872bbdcfa02b9c6d4',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/rabbids-rockstars/en/',
+    osUrl: 'https://opensea.io/collection/rabbids-rockstar'
   },
   {
-    name: "Hanjin Hamsterz Doodles",
+    name: 'Hanjin Hamsterz Doodles',
     chain: 137,
-    address: "0x4c10b3c69ab5f2991ada1a65d9cc93dfe1ae9ac6",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/hamsterz-doodles/en/",
-    osUrl: "https://opensea.io/collection/hanjin-avatars"
+    address: '0x4c10b3c69ab5f2991ada1a65d9cc93dfe1ae9ac6',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/hamsterz-doodles/en/',
+    osUrl: 'https://opensea.io/collection/hanjin-avatars'
   },
   {
-    name: "D10S: The Maradona Collection",
+    name: 'D10S: The Maradona Collection',
     chain: 137,
-    address: "0xe183f1a2763aa1a5dbe6550e02c0f76541a2837c",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/d10s-the-maradona-collection/en/",
-    osUrl: "https://opensea.io/collection/maradona-avatars"
+    address: '0xe183f1a2763aa1a5dbe6550e02c0f76541a2837c',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/d10s-the-maradona-collection/en/',
+    osUrl: 'https://opensea.io/collection/maradona-avatars'
   },
   {
-    name: "The Galaxy Habbos",
+    name: 'The Galaxy Habbos',
     chain: 137,
-    address: "0xc8a86f94af4eb42d58981b36efbe17ebcbf94312",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/galaxy-habbos/en/",
-    osUrl: "https://opensea.io/collection/galaxy-habbos"
+    address: '0xc8a86f94af4eb42d58981b36efbe17ebcbf94312',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/galaxy-habbos/en/',
+    osUrl: 'https://opensea.io/collection/galaxy-habbos'
   },
   {
-    name: "Magnificent Century",
+    name: 'Magnificent Century',
     chain: 137,
-    address: "0x7697cbd9929facdcfe9bb3ba132ecbe0dc368c8b",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/magnificent/en/",
-    osUrl: "https://opensea.io/collection/magnificent-avatars"
+    address: '0x7697cbd9929facdcfe9bb3ba132ecbe0dc368c8b',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/magnificent/en/',
+    osUrl: 'https://opensea.io/collection/magnificent-avatars'
   },
   {
-    name: "Hellbox (2019) Collection",
+    name: 'Hellbox (2019) Collection',
     chain: 137,
-    address: "0xce09bfc137c2a0efdd3246a3804de148ed48ac58",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/hellboy/en/",
-    osUrl: "https://opensea.io/collection/hellboy-avatars"
+    address: '0xce09bfc137c2a0efdd3246a3804de148ed48ac58',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/hellboy/en/',
+    osUrl: 'https://opensea.io/collection/hellboy-avatars'
   },
   {
-    name: "Blastheads",
+    name: 'Blastheads',
     chain: 137,
-    address: "0x02a1fdb33248969123fa96a7d738ec34a9b14683",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/blastheads/en/",
-    osUrl: "https://opensea.io/collection/blastheads"
+    address: '0x02a1fdb33248969123fa96a7d738ec34a9b14683',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/blastheads/en/',
+    osUrl: 'https://opensea.io/collection/blastheads'
   },
   {
-    name: "The STY",
+    name: 'The STY',
     chain: 137,
-    address: "0x2de5990a816c5836b42303c60edb204f5dd95260",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/sty/en/",
-    osUrl: "https://opensea.io/collection/sty-avatars"
+    address: '0x2de5990a816c5836b42303c60edb204f5dd95260',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/sty/en/',
+    osUrl: 'https://opensea.io/collection/sty-avatars'
   },
   {
-    name: "Big Poppa Collection",
+    name: 'Big Poppa Collection',
     chain: 137,
-    address: "0x8ec3f07b8eaf29a9046ac7181b0494fb9f9b1a7e",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/big-poppa-collection/en",
-    osUrl: "https://opensea.io/collection/bigpoppa"
+    address: '0x8ec3f07b8eaf29a9046ac7181b0494fb9f9b1a7e',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/big-poppa-collection/en',
+    osUrl: 'https://opensea.io/collection/bigpoppa'
   },
   {
-    name: "Gucci Cosmos Land",
+    name: 'Gucci Cosmos Land',
     chain: 137,
-    address: "0xd56ceb568e6193476d07933ada89af6b656b2a7c",
-    tsbUrl: "",
-    osUrl: "https://opensea.io/collection/gucci-cosmos-land-avatars"
+    address: '0xd56ceb568e6193476d07933ada89af6b656b2a7c',
+    tsbUrl: '',
+    osUrl: 'https://opensea.io/collection/gucci-cosmos-land-avatars'
   },
   {
-    name: "Captain Laserhawk: A Blood Dragon Remix Collection",
+    name: 'Captain Laserhawk: A Blood Dragon Remix Collection',
     chain: 137,
-    address: "0xd8df5370038270287d1821153529f4e64309f714",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/captain-laserhawk/en",
-    osUrl: "https://opensea.io/collection/captainlaserhawk"
+    address: '0xd8df5370038270287d1821153529f4e64309f714',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/captain-laserhawk/en',
+    osUrl: 'https://opensea.io/collection/captainlaserhawk'
   },
   {
-    name: "The Woo Young-Woo Collection",
+    name: 'The Woo Young-Woo Collection',
     chain: 137,
-    address: "0x75ce22cb29ed5de9a090d6297001f5be8911f0ed",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/extraordinary-attorney-woo/en/",
-    osUrl: "https://opensea.io/collection/extraordinary-attorney-woo"
+    address: '0x75ce22cb29ed5de9a090d6297001f5be8911f0ed',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/extraordinary-attorney-woo/en/',
+    osUrl: 'https://opensea.io/collection/extraordinary-attorney-woo'
   },
   {
-    name: "Shibuya109",
+    name: 'Shibuya109',
     chain: 137,
-    address: "0x1298a8443b70f69e5915cb0778dfb9b45d12eff5",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/shibuya109-fashion-collection/en/",
-    osUrl: "https://opensea.io/collection/shibuya109"
+    address: '0x1298a8443b70f69e5915cb0778dfb9b45d12eff5',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/shibuya109-fashion-collection/en/',
+    osUrl: 'https://opensea.io/collection/shibuya109'
   },
   {
-    name: "Captain Tsubasa Team Collection",
+    name: 'Captain Tsubasa Team Collection',
     chain: 137,
-    address: "0x6f5a269efaeead18637592fef7973bd4b4add392",
-    tsbUrl: "https://sandbox.game/avatar-collections/captain-tsubasa/en",
-    osUrl: "https://opensea.io/collection/captain-tsubasa-avatars"
+    address: '0x6f5a269efaeead18637592fef7973bd4b4add392',
+    tsbUrl: 'https://sandbox.game/avatar-collections/captain-tsubasa/en',
+    osUrl: 'https://opensea.io/collection/captain-tsubasa-avatars'
   },
   {
-    name: "Lamelo Ball",
+    name: 'Lamelo Ball',
     chain: 137,
-    address: "0x3674428d2789a42c7832bc9cba693b51db874455",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/lamelo-ball/en/",
-    osUrl: "https://opensea.io/collection/lameloball-avatars"
+    address: '0x3674428d2789a42c7832bc9cba693b51db874455',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/lamelo-ball/en/',
+    osUrl: 'https://opensea.io/collection/lameloball-avatars'
   },
   {
-    name: "Agorians",
+    name: 'Agorians',
     chain: 137,
-    address: "0x474a3eade5eb2662aa3d34ba74cfb1e39dade361",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/agorians/en/",
-    osUrl: "https://opensea.io/collection/agorians"
+    address: '0x474a3eade5eb2662aa3d34ba74cfb1e39dade361',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/agorians/en/',
+    osUrl: 'https://opensea.io/collection/agorians'
   },
   {
-    name: "Elvis Presley",
+    name: 'Elvis Presley',
     chain: 137,
-    address: "0x26640aaf75e7eea2bf63b3a6af76a4dd46001f30",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/elvis-presley/en/",
-    osUrl: "https://opensea.io/collection/elvis-presley-avatars"
+    address: '0x26640aaf75e7eea2bf63b3a6af76a4dd46001f30',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/elvis-presley/en/',
+    osUrl: 'https://opensea.io/collection/elvis-presley-avatars'
   },
   {
-    name: "The Unity Squad",
+    name: 'The Unity Squad',
     chain: 137,
-    address: "0x65bfd60e84fcc967c909196ae213ca7856107bcb",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/the-unity-squad/en/",
-    osUrl: "https://opensea.io/collection/theunitysquad"
+    address: '0x65bfd60e84fcc967c909196ae213ca7856107bcb',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/the-unity-squad/en/',
+    osUrl: 'https://opensea.io/collection/theunitysquad'
   },
   {
-    name: "Om Nom Cut the Rope (ZeptoLab)",
+    name: 'Om Nom Cut the Rope (ZeptoLab)',
     chain: 137,
-    address: "0x90262e888bbf1f5f375a9286da324f2aeeeebec2",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/zeptolab/en/",
-    osUrl: "https://opensea.io/collection/om-nom-avatars"
+    address: '0x90262e888bbf1f5f375a9286da324f2aeeeebec2',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/zeptolab/en/',
+    osUrl: 'https://opensea.io/collection/om-nom-avatars'
   },
   {
-    name: "Paris Hilton",
+    name: 'Paris Hilton',
     chain: 137,
-    address: "0x1cc25b38d7eb7711e5acc2828d7577cb8a9f2de0",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/paris-hilton/en/",
-    osUrl: "https://opensea.io/collection/paris-hilton-avatars"
+    address: '0x1cc25b38d7eb7711e5acc2828d7577cb8a9f2de0',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/paris-hilton/en/',
+    osUrl: 'https://opensea.io/collection/paris-hilton-avatars'
   },
   {
-    name: "The Wild Ones - DanceFight",
+    name: 'The Wild Ones - DanceFight',
     chain: 137,
-    address: "0xc6054f23bfe8ff4cdc9385fdd2c9e2744bb911e8",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/wild-ones-dancefight/en",
-    osUrl: "https://opensea.io/collection/wild-ones-dancefight"
+    address: '0xc6054f23bfe8ff4cdc9385fdd2c9e2744bb911e8',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/wild-ones-dancefight/en',
+    osUrl: 'https://opensea.io/collection/wild-ones-dancefight'
   },
   {
-    name: "Fist of the North Star",
+    name: 'Fist of the North Star',
     chain: 137,
-    address: "0x5521b00e7952948babc84f052b5d017792784429",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/fist-of-the-north-star/en/",
-    osUrl: "https://opensea.io/collection/fistofthenorthstar-avatars"
+    address: '0x5521b00e7952948babc84f052b5d017792784429',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/fist-of-the-north-star/en/',
+    osUrl: 'https://opensea.io/collection/fistofthenorthstar-avatars'
   },
   {
-    name: "Hell's Kitchen",
+    name: `Hell's Kitchen`,
     chain: 137,
-    address: "0xc3f3ef3929392fdc697c5800d6cd18af73377a8f",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/hells-kitchen/en/",
-    osUrl: "https://opensea.io/collection/hellskitchen-avatars"
+    address: '0xc3f3ef3929392fdc697c5800d6cd18af73377a8f',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/hells-kitchen/en/',
+    osUrl: 'https://opensea.io/collection/hellskitchen-avatars'
   },
   {
-    name: "MadBalls",
+    name: 'MadBalls',
     chain: 137,
-    address: "0x8463b73ea174d04db5fff567c77be968666722b4",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/madballs/en/",
-    osUrl: "https://opensea.io/collection/madballs-avatars"
+    address: '0x8463b73ea174d04db5fff567c77be968666722b4',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/madballs/en/',
+    osUrl: 'https://opensea.io/collection/madballs-avatars'
   },
   {
-    name: "Rabbids Lunar New Year",
+    name: 'Rabbids Lunar New Year',
     chain: 137,
-    address: "0xa090e057a7b3b397efc0f3430659a05b6a41fa40",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/rabbids-lunar-new-year-avatars/",
-    osUrl: "https://opensea.io/collection/rabbids-lunar-new-year-avatars"
+    address: '0xa090e057a7b3b397efc0f3430659a05b6a41fa40',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/rabbids-lunar-new-year-avatars/',
+    osUrl: 'https://opensea.io/collection/rabbids-lunar-new-year-avatars'
   },
   {
-    name: "The Playboy Party People",
+    name: 'The Playboy Party People',
     chain: 137,
-    address: "0x3476190768ddd5bd2dc0fd82b1027281b0f8891f",
-    tsbUrl: "https://www.sandbox.game/avatar-collections/playboy/en",
-    osUrl: "https://opensea.io/collection/the-playboy-party-people"
+    address: '0x3476190768ddd5bd2dc0fd82b1027281b0f8891f',
+    tsbUrl: 'https://www.sandbox.game/avatar-collections/playboy/en',
+    osUrl: 'https://opensea.io/collection/the-playboy-party-people'
   },
   {
-    name: "Care Bears",
+    name: 'Care Bears',
     chain: 137,
-    address: "0x6709660a6237723f278188bcae9e21b21eff8aab",
-    tsbUrl: "https://www.sandbox.game/collections/care-bears/en",
-    osUrl: "https://opensea.io/collection/care-bears-avatars"
+    address: '0x6709660a6237723f278188bcae9e21b21eff8aab',
+    tsbUrl: 'https://www.sandbox.game/collections/care-bears/en',
+    osUrl: 'https://opensea.io/collection/care-bears-avatars'
   },
   {
-    name: "Kuniverse",
+    name: 'Kuniverse',
     chain: 1,
-    address: "0x40eb4f159260a71c262446b10cc5c93d1c1d1d15",
-    tsbUrl: "https://kuniverse.sandbox.game/en",
-    osUrl: "https://opensea.io/collection/kuniverse-avatars"
+    address: '0x40eb4f159260a71c262446b10cc5c93d1c1d1d15',
+    tsbUrl: 'https://kuniverse.sandbox.game/en',
+    osUrl: 'https://opensea.io/collection/kuniverse-avatars'
   },
   {
-    name: "Steve Aoki",
+    name: 'Steve Aoki',
     chain: 1,
-    address: "0xc7170a6c9218bfac05a5cd3d40003b0d04e8750a",
-    tsbUrl: "https://www.sandbox.game/en/steve-aoki/",
-    osUrl: "https://opensea.io/collection/steve-aoki-avatars"
+    address: '0xc7170a6c9218bfac05a5cd3d40003b0d04e8750a',
+    tsbUrl: 'https://www.sandbox.game/en/steve-aoki/',
+    osUrl: 'https://opensea.io/collection/steve-aoki-avatars'
   },
   {
-    name: "Gym A Club (Olivex)",
+    name: 'Gym A Club (Olivex)',
     chain: 1,
-    address: "0x92bce4a82f1407c711b889e34fe8cb9b49f02217",
-    tsbUrl: "https://olivex.sandbox.game/",
-    osUrl: "https://opensea.io/collection/gym-a-club"
+    address: '0x92bce4a82f1407c711b889e34fe8cb9b49f02217',
+    tsbUrl: 'https://olivex.sandbox.game/',
+    osUrl: 'https://opensea.io/collection/gym-a-club'
   },
   {
-    name: "The People of Crypto (PoC)",
+    name: 'The People of Crypto (PoC)',
     chain: 1,
-    address: "0xe36f5350b097ea72e561ee154b45363c808a56d2",
-    tsbUrl: "https://www.sandbox.game/en/people-of-crypto/",
-    osUrl: "https://opensea.io/collection/peopleofcrypto"
+    address: '0xe36f5350b097ea72e561ee154b45363c808a56d2',
+    tsbUrl: 'https://www.sandbox.game/en/people-of-crypto/',
+    osUrl: 'https://opensea.io/collection/peopleofcrypto'
   },
   {
-    name: "The Doggies (Snoop Dogg)",
+    name: 'The Doggies (Snoop Dogg)',
     chain: 1,
-    address: "0xc7df86762ba83f2a6197e1ff9bb40ae0f696b9e6",
-    tsbUrl: "https://www.sandbox.game/en/snoopdogg/",
-    osUrl: "https://opensea.io/collection/snoop-dogg-doggies"
+    address: '0xc7df86762ba83f2a6197e1ff9bb40ae0f696b9e6',
+    tsbUrl: 'https://www.sandbox.game/en/snoopdogg/',
+    osUrl: 'https://opensea.io/collection/snoop-dogg-doggies'
   }
 ]
